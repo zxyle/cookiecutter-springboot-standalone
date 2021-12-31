8456b3304c288c3726e8db1f5a5543f0683b967d
Xiang Zheng <zxyful@gmail.com>
2021-12-31 21:45:24 +0800

    Complete the spring boot basic template

---
+++ {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/{{ cookiecutter.app_name }}Application.java
@@ -0,0 +1,13 @@
+package {{ cookiecutter.group }}.{{ cookiecutter.artifact.replace('-', '') }};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class {{ cookiecutter.app_name }}Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run({{ cookiecutter.app_name }}Application.class, args);
+  }
+
+}
