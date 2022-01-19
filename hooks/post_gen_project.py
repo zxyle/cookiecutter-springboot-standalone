c6bfe98298c61e71c65cb14e87ac5e580cb40c89
Xiang Zheng <zxyful@gmail.com>
2022-01-19 23:28:58 +0800

    Already supports gradle build tool.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,33 @@
 import os
+from typing import List
+
+
+def remove_files(files: List[str]):
+    for file_name in files:
+        os.remove(file_name)
 
 
 def remove_open_source_files():
     file_names = ["LICENSE"]
-    for file_name in file_names:
-        os.remove(file_name)
+    remove_files(file_names)
+
+
+def remove_build_tool(tool: str):
+    file_names = []
+    if tool == "maven":
+        file_names.extend(["build.gradle", "settings.gradle"])
+    else:
+        file_names.extend(["pom.xml"])
+
+    remove_files(file_names)
 
 
 def main():
     if "{{ cookiecutter.open_source_license }}" == "Not open source":
         remove_open_source_files()
+
+    tool = "maven"
+    remove_build_tool(tool)
 
 
 if __name__ == "__main__":
