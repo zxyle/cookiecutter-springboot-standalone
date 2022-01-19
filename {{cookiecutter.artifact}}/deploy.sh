80bde61cd249a2fc4331cad90c0955356623d23b
Xiang Zheng <zxyful@gmail.com>
2022-01-19 21:23:52 +0800

    Improve deployment documentation

--- {{cookiecutter.artifact}}/deploy.sh
+++ {{cookiecutter.artifact}}/deploy.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
 
+# 部署服务器
+server=root@127.0.0.1
+# 部署目录
+dir=/opt/webapps/{{ cookiecutter.artifact }}/
+
 function package() {
     mvn clean package -Dmaven.test.skip=true
 }
+
+function restart() {
+    ssh $server "systemctl restart {{ cookiecutter.artifact }}/"
+}
+
+function publish() {
+    package
+    # TODO 增加jar包备份
+    scp -rvC $target/*.jar $server:$dir
+    restart
+}
+
+
+function usage() {
+    echo "Usage: sh deploy.sh [package|publish]"
+    exit 0
+}
+
+
+#根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+  "package")
+    package
+    ;;
+  "publish")
+    publish
+    ;;
+  *)
+    usage
+    ;;
+esac
