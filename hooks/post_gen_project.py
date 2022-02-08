dd05dd28e7f7fc0786bc92434384f401a4623c60
Xiang Zheng <zxyful@gmail.com>
2022-02-08 22:37:52 +0800

    git init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -23,12 +23,23 @@ def remove_build_tool(tool: str):
     remove_files(file_names)
 
 
+def git_init():
+    cmd1 = 'git config --local user.name "{{ cookiecutter.author_name }}"'
+    cmd2 = 'git config --local user.email "{{ cookiecutter.email }}"'
+    try:
+        os.system(cmd1)
+        os.system(cmd2)
+    except:
+        pass
+
+
 def main():
     if "{{ cookiecutter.open_source_license }}" == "Not open source":
         remove_open_source_files()
 
     tool = "maven"
     remove_build_tool(tool)
+    git_init()
 
 
 if __name__ == "__main__":
