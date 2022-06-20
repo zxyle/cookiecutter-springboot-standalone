edec9ef2449f9e68931f160faeead4943a6b2c50
郑翔 <zxyful@gmail.com>
2022-06-20 14:09:48 +0800

    Add deserialization array function

--- {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/common/util/JacksonUtil.java
+++ {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/common/util/JacksonUtil.java
@@ -3,11 +3,19 @@ package {{ cookiecutter.basePackage }}.common.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * json工具类
+ */
 public class JacksonUtil {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    // 反序列化
+    /**
+     * 反序列化
+     */
     public static <T> T deserialize(String body, Class<T> t) {
         T response;
         try {
@@ -20,7 +28,22 @@ public class JacksonUtil {
         return null;
     }
 
-    // 序列化
+    /**
+     * 反序列化数组
+     */
+    public static <T> List<T> deserializeArray(String body) {
+        List<T> list = new ArrayList<>();
+        try {
+            list = objectMapper.readValue(body, List.class);
+            return list;
+        } catch (JsonProcessingException e) {
+            return list;
+        }
+    }
+
+    /**
+     * 序列化
+     */
     public static String serialize(Object obj) {
         String o;
         try {
