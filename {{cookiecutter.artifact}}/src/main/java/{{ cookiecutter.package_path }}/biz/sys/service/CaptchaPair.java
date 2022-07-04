c3f937e050e43d6d9c614dc6c563912cd8df48b3
Xiang Zheng <zxyful@gmail.com>
2022-07-04 10:08:09 +0800

    This feature has been developed. #12

--- {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/biz/sys/service/CaptchaPair.java
+++ {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/biz/sys/service/CaptchaPair.java
@@ -3,14 +3,17 @@ package {{ cookiecutter.basePackage }}.biz.sys.service;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
 @Data
 @AllArgsConstructor
 public class CaptchaPair {
 
     /**
-     * base64格式图片
+     * 验证码图片字节数组
      */
-    private String b64Image;
+    private ByteArrayOutputStream byteArrayOutputStream;
 
     /**
      * 验证码结果
@@ -18,7 +21,16 @@ public class CaptchaPair {
     private String code;
 
     /**
-     * 验证码字节数组
+     * 获取验证码图片Base64编码
      */
-    private byte[] bytes;
+    public String getB64Image() {
+        return Base64.getEncoder().encodeToString(getBytes());
+    }
+
+    /**
+     * 获取验证码图片字节数组
+     */
+    public byte[] getBytes() {
+        return byteArrayOutputStream.toByteArray();
+    }
 }
