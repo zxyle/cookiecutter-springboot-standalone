31dc2785b7b0d0a0c3c822d04ef88790d7e59cb1
Xiang Zheng <zxyful@gmail.com>
2022-07-04 10:06:50 +0800

    This feature has been developed. #13

--- {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/biz/sys/controller/FriendlyUrlController.java
+++ {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/biz/sys/controller/FriendlyUrlController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import {{ cookiecutter.basePackage }}.biz.sys.service.IFriendlyUrlService;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -38,5 +39,47 @@ public class FriendlyUrlController {
         List<FriendlyUrl> urls = thisService.list(wrapper);
         return new ApiResponse<>(urls);
     }
+
+    /**
+     * 添加友链
+     */
+    @PostMapping("/urls")
+    public ApiResponse<FriendlyUrl> add(@Valid @RequestBody FriendlyUrl entity) {
+        thisService.save(entity);
+        return new ApiResponse<>(entity);
+    }
+
+    /**
+     * 删除友链
+     *
+     * @param id 友链ID
+     */
+    @DeleteMapping("/urls/{id}")
+    public ApiResponse<FriendlyUrl> delete(@PathVariable("id") Long id) {
+        FriendlyUrl entity = thisService.getById(id);
+        if (entity == null) {
+            return new ApiResponse<>("友链不存在", false);
+        }
+        thisService.removeById(id);
+        return new ApiResponse<>("删除成功", true);
+    }
+
+    /**
+     * 更新友链
+     *
+     * @param id     友链ID
+     * @param entity 友链实体
+     * @return 友链实体
+     */
+    @PutMapping("/urls/{id}")
+    public ApiResponse<FriendlyUrl> update(@PathVariable("id") Long id, @Valid @RequestBody FriendlyUrl entity) {
+        FriendlyUrl oldEntity = thisService.getById(id);
+        if (oldEntity == null) {
+            return new ApiResponse<>("友链不存在", false);
+        }
+        thisService.updateById(entity);
+        return new ApiResponse<>(entity);
+    }
+
 
 }
