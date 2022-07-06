58c43abbed37a01ba3d41e4671cc20d041104aa8
Xiang Zheng <zxyful@gmail.com>
2022-07-06 10:54:07 +0800

    This feature has been developed. #14

---
+++ {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/common/util/RateUtil.java
@@ -0,0 +1,46 @@
+package {{ cookiecutter.basePackage }}.biz.project.util;
+
+import java.text.NumberFormat;
+
+/**
+ * 百分比计算工具类
+ */
+public class RateUtil {
+
+    // 设置精确到小数点后2位
+    public static final int DIGIT = 2;
+
+    /**
+     * 计算百分比
+     * 4 / 20 = 20%
+     *
+     * @param divisor  除数
+     * @param dividend 被除数
+     * @return 带百分号的百分数
+     */
+    public static String cal(double divisor, double dividend, int digit) {
+        if (dividend <= 0 || divisor <= 0) {
+            return "0%";
+        }
+
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setMaximumFractionDigits(digit);
+        // numberFormat.setMinimumFractionDigits(DIGIT);
+        String result = numberFormat.format(divisor / dividend * 100);
+        return result + "%";
+    }
+
+    public static String cal(double divisor, double dividend) {
+        return cal(divisor, dividend, DIGIT);
+    }
+
+    public static String cal(float divisor, float dividend) {
+        return cal(divisor, dividend, DIGIT);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RateUtil.cal(10, 3,2));
+    }
+
+}
+
