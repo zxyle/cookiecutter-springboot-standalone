a60ab26e2e5986cdb5354958c52c9485e4b0deff
Xiang Zheng <zxyful@gmail.com>
2022-09-11 16:23:53 +0800

    Update init sql file

--- {{cookiecutter.artifact}}/docs/ddl/auth.sql
+++ {{cookiecutter.artifact}}/docs/ddl/auth.sql
@@ -21,7 +21,7 @@ CREATE TABLE `auth_group` (
 -- Records of auth_group
 -- ----------------------------
 BEGIN;
-INSERT INTO `auth_group` (`id`, `create_time`, `update_time`, `name`, `parent_id`, `sort_no`, `description`) VALUES (1, '2022-07-04 14:04:28', '2022-07-04 14:10:21', 'root', 0, 1, '根用户组');
+INSERT INTO `auth_group` (`name`, `parent_id`, `sort_no`, `description`) VALUES ('root', 0, 1, '根用户组');
 COMMIT;
 
 -- ----------------------------
@@ -83,24 +83,24 @@ CREATE TABLE `auth_permission` (
 -- Records of auth_permission
 -- ----------------------------
 BEGIN;
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (1, '2022-06-09 09:41:02', '2022-07-04 14:06:38', '上帝权限', '*', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (2, '2022-06-14 16:37:46', '2022-07-04 14:06:31', '用户组查询', 'auth:groups:list', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (3, '2022-06-14 16:38:25', '2022-07-04 14:06:31', '获取用户组树状结构', 'auth:groups:tree', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (4, '2022-06-14 16:39:23', '2022-07-04 14:06:31', '新增用户组', 'auth:groups:add', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (5, '2022-06-14 16:39:37', '2022-07-04 14:06:32', '按ID查询用户组', 'auth:groups:get', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (6, '2022-06-14 16:39:54', '2022-07-04 14:06:32', '按ID更新用户组', 'auth:groups:update', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (7, '2022-06-14 16:40:20', '2022-07-04 14:06:32', '按ID删除用户组', 'auth:groups:delete', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (8, '2022-06-14 16:40:53', '2022-07-04 14:06:32', '用户组迁移', 'auth:groups:migrate', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (9, '2022-06-14 16:41:08', '2022-07-04 14:06:32', '角色查询', 'auth:roles:list', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (10, '2022-06-14 16:41:24', '2022-07-04 14:06:32', '新增角色', 'auth:roles:add', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (11, '2022-06-14 16:42:06', '2022-07-04 14:06:32', '按ID查询角色', 'auth:roles:get', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (12, '2022-06-14 16:42:28', '2022-07-04 14:06:33', '按ID更新角色', 'auth:roles:update', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (13, '2022-06-22 13:36:38', '2022-07-04 14:06:33', '按ID删除角色', 'auth:roles:delete', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (14, '2022-07-04 14:07:20', '2022-07-04 14:08:48', '查询用户拥有所有权限信息', 'auth:permissions:list', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (15, '2022-07-04 14:07:47', '2022-07-04 14:07:47', '新增权限', 'auth:permissions:add', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (16, '2022-07-04 14:08:04', '2022-07-04 14:08:04', '按ID查询权限', 'auth:permissions:get', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (17, '2022-07-04 14:08:19', '2022-07-04 14:08:19', '按ID更新权限', 'auth:permissions:update', NULL);
-INSERT INTO `auth_permission` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (18, '2022-07-04 14:08:38', '2022-07-04 14:08:38', '按ID删除权限', 'auth:permissions:delete', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('上帝权限', '*', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('用户组查询', 'auth:groups:list', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('获取用户组树状结构', 'auth:groups:tree', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('新增用户组', 'auth:groups:add', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID查询用户组', 'auth:groups:get', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID更新用户组', 'auth:groups:update', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID删除用户组', 'auth:groups:delete', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('用户组迁移', 'auth:groups:migrate', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('角色查询', 'auth:roles:list', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('新增角色', 'auth:roles:add', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID查询角色', 'auth:roles:get', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID更新角色', 'auth:roles:update', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID删除角色', 'auth:roles:delete', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('查询用户拥有所有权限信息', 'auth:permissions:list', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('新增权限', 'auth:permissions:add', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID查询权限', 'auth:permissions:get', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID更新权限', 'auth:permissions:update', NULL);
+INSERT INTO `auth_permission` (`name`, `code`, `description`) VALUES ('按ID删除权限', 'auth:permissions:delete', NULL);
 COMMIT;
 
 -- ----------------------------
@@ -122,10 +122,10 @@ CREATE TABLE `auth_role` (
 -- Records of auth_role
 -- ----------------------------
 BEGIN;
-INSERT INTO `auth_role` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (1, '2022-06-14 16:35:04', '2022-06-21 23:31:59', '上帝角色', 'god', '拥有系统所有权限');
-INSERT INTO `auth_role` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (2, '2022-06-20 13:45:34', '2022-06-20 13:45:34', '管理员', 'admin', NULL);
-INSERT INTO `auth_role` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (3, '2022-06-21 23:28:54', '2022-06-21 23:40:15', '来宾', 'guest', '基本登录、登出、密码管理');
-INSERT INTO `auth_role` (`id`, `create_time`, `update_time`, `name`, `code`, `description`) VALUES (4, '2022-06-21 23:29:17', '2022-06-21 23:31:18', '只读角色', 'readonly', '所有资源只读，无法修改删除和更新');
+INSERT INTO `auth_role` (`name`, `code`, `description`) VALUES ('上帝角色', 'god', '拥有系统所有权限');
+INSERT INTO `auth_role` (`name`, `code`, `description`) VALUES ('管理员', 'admin', NULL);
+INSERT INTO `auth_role` (`name`, `code`, `description`) VALUES ('来宾', 'guest', '基本登录、登出、密码管理');
+INSERT INTO `auth_role` (`name`, `code`, `description`) VALUES ('只读角色', 'readonly', '所有资源只读，无法修改删除和更新');
 COMMIT;
 
 -- ----------------------------
@@ -173,7 +173,7 @@ CREATE TABLE `auth_user` (
 -- Records of auth_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `auth_user` (`id`, `create_time`, `update_time`, `login_name`, `pwd`, `real_name`, `mobile`, `email`, `telephone`, `user_lock`, `is_super`, `expire_time`) VALUES (1, '2022-06-08 22:19:10', '2022-06-09 21:32:41', 'zhengxiang', '$2a$10$rgOjIXgsgQnox3GcCV7L2.aVNcDaXqgzKC.GWDu3PwGF12H83qfdi', '郑翔', '13111111111', 'me@example.com', '057112345678', 0, 0, '2099-12-31 23:59:59');
+INSERT INTO `auth_user` (`login_name`, `pwd`, `real_name`, `mobile`, `email`, `telephone`, `user_lock`, `is_super`, `expire_time`) VALUES ('admin', '$2a$10$rgOjIXgsgQnox3GcCV7L2.aVNcDaXqgzKC.GWDu3PwGF12H83qfdi', 'admin', '13111111111', 'me@example.com', '057112345678', 0, 0, '2099-12-31 23:59:59');
 COMMIT;
 
 -- ----------------------------
@@ -214,7 +214,7 @@ CREATE TABLE `auth_user_permission` (
 -- Records of auth_user_permission
 -- ----------------------------
 BEGIN;
-INSERT INTO `auth_user_permission` (`id`, `create_time`, `update_time`, `user_id`, `permission_id`) VALUES (1, '2022-06-16 06:57:17', '2022-07-04 13:49:06', 1, 1);
+INSERT INTO `auth_user_permission` (`user_id`, `permission_id`) VALUES (1, 1);
 COMMIT;
 
 -- ----------------------------
