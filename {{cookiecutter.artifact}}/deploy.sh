83552e810bd3cc3c7e689eba2e18190ff15a476e
zxyle <zxyful@gmail.com>
2023-04-28 15:30:20 +0800

    部署脚本增加初始化环境功能

--- {{cookiecutter.artifact}}/deploy.sh
+++ {{cookiecutter.artifact}}/deploy.sh
@@ -4,6 +4,7 @@
 server=root@127.0.0.1
 # 部署目录
 dir=/opt/webapps/{{ cookiecutter.artifact.replace('-api', '') }}/{{ cookiecutter.artifact }}/
+frontend_dir=/opt/webapps/{{ cookiecutter.artifact.replace('-api', '') }}/{{ cookiecutter.artifact.replace('-api', '-web') }}/
 jar={{ cookiecutter.artifact }}-{{ cookiecutter.version }}.jar
 
 # 拉取代码并打包
@@ -47,6 +48,33 @@ function usage() {
     exit 0
 }
 
+# 初始化前后端运行环境
+function init() {
+    package
+    ssh $server "mkdir -p $dir"
+    rsync -avz --progress --delete ./target/lib/ $server:$dir/lib
+    scp -rvC ./target/*.jar $server:$dir
+    scp -rvC ./deploy/* $server:$dir
+    scp -rvC ./src/main/resources/application.properties $server:$dir/config
+    scp -rvC ./src/main/resources/application-prod.properties $server:$dir/config
+    scp -rvC ./src/main/resources/logback-spring.xml $server:$dir/config
+    scp -rvC ./target/lib $server:$dir
+    ssh $server "mv $dir/*.service /etc/systemd/system"
+    ssh $server "systemctl daemon-reload"
+    ssh $server "systemctl enable {{ cookiecutter.artifact }} --now"
+
+    # 前端配置
+    ssh $server "mkdir -p $frontend_dir"
+    ssh $server "mv $dir/index.html $frontend_dir/"
+
+    # Nginx配置
+    ssh $server "systemctl enable nginx --now"
+    ssh $server "mv $dir/*.conf /etc/nginx/conf.d/"
+    ssh $server "nginx -t"
+    ssh $server "nginx -s reload"
+    # TODO 80端口冲突，需要删除nginx.conf中的80端口配置
+}
+
 
 #根据输入参数，选择执行对应方法，不输入则执行使用说明
 case "$1" in
@@ -61,6 +89,9 @@ case "$1" in
     ;;
   "rollback")
     rollback
+    ;;
+  "init")
+    init
     ;;
   *)
     usage
