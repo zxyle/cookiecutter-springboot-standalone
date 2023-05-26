e0d202dbb3f0525c00d9afa2ef76960230ba3270
zxyle <zxyful@gmail.com>
2023-05-26 13:52:30 +0800

    修改实践中产生的bug

--- {{cookiecutter.artifact}}/docs/ddl/auth.sql
+++ {{cookiecutter.artifact}}/docs/ddl/auth.sql
@@ -98,7 +98,7 @@ CREATE TABLE `auth_permission` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '权限名称',
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '权限名称',
   `code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '权限代码',
   `description` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '描述信息',
   `parent_id` bigint unsigned NOT NULL DEFAULT '1' COMMENT '父级权限ID',
