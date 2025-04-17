cf2474ce154557e0ead2a6e690e86df9c7a85929
zxyle <zxyful@gmail.com>
2025-04-17 21:33:38 +0800

    登录时对参数进行去空格处理

--- {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/biz/auth/login/AccountLoginRequest.java
+++ {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/biz/auth/login/AccountLoginRequest.java
@@ -1,11 +1,13 @@
 package {{ cookiecutter.basePackage }}.biz.auth.login;
 
+import org.apache.commons.lang3.StringUtils;
 import {{ cookiecutter.basePackage }}.common.request.BaseRequest;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.Length;
 
 import {{ cookiecutter.namespace }}.validation.constraints.NotBlank;
+import {{ cookiecutter.namespace }}.validation.constraints.Pattern;
 
 /**
  * 使用注册账号(用户名/邮箱/手机号) + 密码方式登录
@@ -37,6 +39,7 @@ public class AccountLoginRequest extends BaseRequest {
      * @mock 1a38695e
      */
     @Length(min = 8, max = 8, message = "验证码ID长度为8个字符")
+    @Pattern(regexp = "^[0-9a-z]{8}$", message = "验证码ID格式不正确")
     private String captchaId;
 
     /**
@@ -46,5 +49,18 @@ public class AccountLoginRequest extends BaseRequest {
      */
     @Length(min = 4, max = 6, message = "验证码结果长度为4-6个字符")
     private String code;
+
+
+    public String getAccount() {
+        return StringUtils.trim(account);
+    }
+
+    public String getPassword() {
+        return StringUtils.trim(password);
+    }
+
+    public String getCode() {
+        return StringUtils.trim(code);
+    }
 
 }
