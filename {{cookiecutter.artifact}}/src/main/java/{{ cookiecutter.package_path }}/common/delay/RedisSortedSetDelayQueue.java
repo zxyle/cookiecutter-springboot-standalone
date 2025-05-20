62e7eab835d1807ba4756bd540e6a2dea4694e49
zxyle <zxyful@gmail.com>
2025-05-20 20:48:09 +0800

    BASE_TIMESTAMP 动态生成

--- {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/common/delay/RedisSortedSetDelayQueue.java
+++ {{cookiecutter.artifact}}/src/main/java/{{ cookiecutter.package_path }}/common/delay/RedisSortedSetDelayQueue.java
@@ -59,7 +59,7 @@ public class RedisSortedSetDelayQueue {
      * 基准点秒级时间戳，用来显著降低 zset score 数值大小，利于 Redis 存储优化
      */
     private static final long BASE_TIMESTAMP =
-            LocalDateTime.of(2025, 5, 18, 0, 0, 0)
+            LocalDateTime.of({% now 'local', '%Y, %m, %d' %}, 0, 0, 0)
                     .atZone(ZoneId.systemDefault())
                     .toEpochSecond();
 
